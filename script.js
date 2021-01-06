ba1c4a899b73c770978c3be6f65f04fb62e46c5e
Praveen Oruganti <praveenorugantitech@gmail.com>
2021-01-07 01:57:45 +0530

    All About JavaScript

--- script.js
+++ script.js
@@ -1,36 +1,42 @@
-const openModalButtons = document.querySelectorAll('[data-modal-target]')
-const closeModalButtons = document.querySelectorAll('[data-close-button]')
-const overlay = document.getElementById('overlay')
+'use strict';
 
-openModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = document.querySelector(button.dataset.modalTarget)
-    openModal(modal)
-  })
-})
 
-overlay.addEventListener('click', () => {
-  const modals = document.querySelectorAll('.modal.active')
-  modals.forEach(modal => {
-    closeModal(modal)
-  })
-})
+const modal =document.querySelector('.modal');
+const closeBtn =document.querySelector('.close-modal');
+const overlay =document.querySelector('.overlay');
+const buttonModel = document.querySelectorAll('.show-modal');
 
-closeModalButtons.forEach(button => {
-  button.addEventListener('click', () => {
-    const modal = button.closest('.modal')
-    closeModal(modal)
-  })
-})
-
-function openModal(modal) {
-  if (modal == null) return
-  modal.classList.add('active')
-  overlay.classList.add('active')
+/* */
+const openModel = function(){
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
 }
 
-function closeModal(modal) {
-  if (modal == null) return
-  modal.classList.remove('active')
-  overlay.classList.remove('active')
+/* if you want to operate same function in multiple elements then it's always good 
+to write a separate function and call it when needed */
+const closeModel = function(){
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
 }
+
+/* We are not using () after the function openModel and closeModel because 
+we only want them to execute once the click event happens*/
+
+for(let i=0; i<buttonModel.length;i++){
+    buttonModel[i].addEventListener('click',openModel);
+}
+closeBtn.addEventListener('click',closeModel);
+overlay.addEventListener('click',closeModel);
+
+
+//KeyPress Button ( Esc key)
+// Keyboard events are global events. keypress,keydown,keyup
+
+document.addEventListener('keydown',function(event){
+   /*  console.log(event.which); it's telling which key has been pressed */ 
+    /*console.log(event); return the keyboard object and all the properties  */
+   if(event.key==='Escape' && !modal.classList.contains('hidden')){
+            closeModel();
+     }
+  });
+
